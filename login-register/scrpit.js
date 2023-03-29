ca84db4e2460d669ccbc5811a87e66f1290af876
wandeadeyeye <54033933+wandeadeyeye@users.noreply.github.com>
2023-03-29 15:20:52 +0100

    scrpit-modified

--- login-register/scrpit.js
+++ login-register/scrpit.js
@@ -1,15 +1,11 @@
-let wrapper = document.querySelector('.wrapper');
-let loginLink = document.querySelector('.login-link');
-let registerLink = document.querySelector('.register-link');
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
 
-if (registerLink){
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
 });
-}
 
-if (loginLink){
 loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
 });
-}
